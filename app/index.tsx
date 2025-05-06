77e3af0949d2a2374be2265dc82deb2d9c4b39c7
Yordy <iYord@protonmail.com>
2025-05-06 11:24:53 -0400

    init

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-600 font-bold text-5xl">Hola</Text>
+    </View>
+  );
+}
