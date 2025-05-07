97151504d296816ff013d4a4a045b16e52c1f5b4
Yordy <iYord@protonmail.com>
2025-05-07 11:39:19 -0400

    Navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-600 font-bold text-5xl">Hola</Text>
+      <Text className="text-blue-600 font-bold text-5xl">Home</Text>
     </View>
   );
 }
